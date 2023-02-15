91d6ba1f03f6a8878b80dc1613f82656a27196b8
erickoliveira <erick.oliveira9607@gmail.com>
2023-02-15 18:00:17 -0300

    teste

--- src/main/java/com/example/SnmpAgent/converters/WindowsConverter.java
+++ src/main/java/com/example/SnmpAgent/converters/WindowsConverter.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class WindowsConverter {
     WindowsObject windows = new WindowsObject();
     SystemInfo si = new SystemInfo();
